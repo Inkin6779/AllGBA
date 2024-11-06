880ecf8e59502a90f43681d4ba869eb5964c4c3b
Markl Robbins <inkinbuddies@gmail.com>
2024-11-06 13:37:32 -0600

    Generations has both lol

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -32,7 +32,6 @@ var games = {
     "pokemonfirered":"Pokemon Fire Red",
     "pokemonpinball":"Pokemon Pinball",
     "pokemonpinballgenii":"Pokemon Pinball (Generations)",
-    "pokemonpinballgeni":"Pokemon Pinball (Original)",
     "pokemonxy":"Pokemon X and Y for GBA",
     "pokemonunbound":"Pokemon Unbound",
     "pokemonultimatefusion":"Pokemon Ultimate Fusion",
